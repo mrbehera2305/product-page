05331d8c1af29aedbaa7b16ff572634f0e5899cb
Manoranjan <mrbehera2305@gmail.com>
2024-08-18 00:06:21 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const menuToggle = document.getElementById('menuToggle');
+  const navLinks = document.getElementById('navLinks');
+
+  menuToggle.addEventListener('click', () => {
+      navLinks.classList.toggle('active');
+  });
+});
